598b4b59970bc1c2ccc37c04134a7bb0110bc283
https://github.com/1471436961 <1471436961@qq.com>
2026-05-16 23:34:10 +0800

    add logs

---
+++ logs/run-frontend.ps1
@@ -0,0 +1,2 @@
+﻿Set-Location 'E:\Bronchoscopy-AI-Intelligent-Assisted-Diagnosis-System\frontend'
+npm run dev -- --host 0.0.0.0
---
+++ logs/run-inference.ps1
@@ -0,0 +1,5 @@
+﻿$env:PYTHONDONTWRITEBYTECODE='1'
+$env:MODEL_PATH=''
+$env:PYTHONPATH='E:\Bronchoscopy-AI-Intelligent-Assisted-Diagnosis-System;E:\Bronchoscopy-AI-Intelligent-Assisted-Diagnosis-System\inference;E:\Bronchoscopy-AI-Intelligent-Assisted-Diagnosis-System\backend\generated'
+Set-Location 'E:\Bronchoscopy-AI-Intelligent-Assisted-Diagnosis-System'
+& 'E:\Bronchoscopy-AI-Intelligent-Assisted-Diagnosis-System\.venv-1\Scripts\python.exe' -m app.server
